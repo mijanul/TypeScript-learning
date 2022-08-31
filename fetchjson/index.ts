6d4ca13c217b33e0222961cfd5c3af4b456a50d0
MijanuL <skmijanulh@gmail.com>
2022-08-31 12:18:42 +0530

    first interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,17 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  const todo = response.data;
+  const { id, title, completed }: Todo = response.data;
+
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}`);
 });
